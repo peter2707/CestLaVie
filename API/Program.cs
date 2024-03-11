7d8550c8d0c67474208943be44c516ee0205ea66
peter2707 <prummonkolsophearith@gmail.com>
2024-03-11 23:00:19 +1100

    created db migration, api controller, async function for get user

--- API/Program.cs
+++ API/Program.cs
@@ -2,11 +2,32 @@ using API.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add controllers services
+builder.Services.AddControllers(); // Add this line to configure controllers services
+
 var app = builder.Build();
 
-builder.Services.AddDbContext<DataContext>(opt => 
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
 app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+// Map API controllers
+app.MapControllers(); // Add this line to map API controllers
+
 app.Run();
