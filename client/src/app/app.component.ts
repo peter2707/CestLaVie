203476869fbb114746d4eb9c6a3d480d77848873
peter2707 <prummonkolsophearith@gmail.com>
2024-03-16 22:38:26 +1100

    added navbar, register form, login form, login persisting, api integration for login/register

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,24 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './_services/account.service';
+import { User } from './_model/user';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
-  title = "C'est La Vie";
-  users: any;
+	title = "C'est La Vie";
 
-  constructor(private http: HttpClient) {
+	constructor(private accountService: AccountService) {}
 
-  }
+	ngOnInit(): void {
+		this.setCurrentUser();
+	}
 
-  ngOnInit(): void {
-    this.http.get('https://localhost:5278/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed')
-    });
-  }
+	setCurrentUser() {
+		const userString = localStorage.getItem('user');
+		if(!userString) return;
+		const user: User = JSON.parse(userString);
+		this.accountService.setCurrentUser(user);
+	}
 }
